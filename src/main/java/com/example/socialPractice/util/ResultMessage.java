9e6ebcba970a4c54489dae3ca1a98a7426de9a0b
jane <779761563@qq.com>
2019-01-25 15:44:09 +0800

    jane's test

--- src/main/java/com/example/socialPractice/util/ResultMessage.java
+++ src/main/java/com/example/socialPractice/util/ResultMessage.java
@@ -1,7 +1,7 @@
 package com.example.socialPractice.util;
 
 public enum ResultMessage {
-    SUCCESS,
-    FAIL
+    Success,
+    Fail
 
 }
---
+++ src/main/java/com/example/socialPractice/util/Status.java
@@ -0,0 +1,7 @@
+package com.example.socialPractice.util;
+
+public enum Status {
+    Student,
+    FacultyLeader,//院系负责人
+    GeneralLeader//总负责人
+}
